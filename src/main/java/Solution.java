61fbd8521bf907d5108039562fd07b09e1925afe
Lloyd Perez <lloyd4965jr@yahoo.com>
2022-10-18 17:57:22 -0400

    completed MergeTwoSortList

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -89,3 +89,46 @@ class ValidPalindrome {
         return true;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//class MergeTwoSortedList {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode dummyNode = new ListNode();
+//        ListNode tail = dummyNode;
+//        while (list1 != null && list2 != null) {
+//            if (list1.val < list2.val) {
+//                tail.next = list1;
+//                list1 = list1.next;
+//            } else {
+//                tail.next = list2;
+//                list2 = list2.next;
+//                tail = tail.next;
+//
+//            }
+//            tail = tail.next;
+//        }
+//        tail.next = list1 != null ? list1 : list2;
+//        return tail.next;
+//    }
+//}
+
+// if (list1 == null) return list2;
+//         if (list2 == null) return list1;
+//
+//         if (list1.val < list2.val) {
+//        list1.next = mergeTwoLists(list1.next, list2);
+//        return list1;
+//        } else {
+//        list2.next = mergeTwoLists(list2.next, list1);
+//        return list2;
+//        }
+//        }
