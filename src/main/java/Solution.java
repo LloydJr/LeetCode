b9fa273149c6bf955eceae26750232f8e91603a4
Lloyd Perez <lloyd4965jr@yahoo.com>
2022-11-22 13:52:30 -0500

    LinkedListCycle

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -242,6 +242,20 @@ class MaximumDepthofBinaryTree {
     }
 }
 
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
+
 
 
 
